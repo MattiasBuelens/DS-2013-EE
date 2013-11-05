bf1dac9c68c954170572ef64561d09ec1ab8152b
vital-dhaveloose <vital.dhaveloose@hotmail.be>
2013-11-05 12:13:54 +0100

    working on the client: loading cars via managerSession

--- CarRental-client/src/java/client/Main.java
+++ CarRental-client/src/java/client/Main.java
@@ -1,7 +1,13 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.StringTokenizer;
 import javax.naming.InitialContext;
 import rental.CarType;
 import rental.Reservation;
@@ -18,6 +24,40 @@ public class Main extends AbstractScriptedTripTest<CarRentalSessionRemote, Manag
     public static void main(String[] args) throws Exception {
         //TODO: use updated manager interface to load cars into companies
         new Main("trips").run();
+    }
+    
+    public static List<Car> loadData(String datafile)
+            throws NumberFormatException, IOException {
+
+        List<Car> cars = new LinkedList<Car>();
+
+        int nextuid = 0;
+
+        //open file from jar
+        BufferedReader in = new BufferedReader(new InputStreamReader(RentalStore.class.getClassLoader().getResourceAsStream(datafile)));
+        //while next line exists
+        while (in.ready()) {
+            //read line
+            String line = in.readLine();
+            //if comment: skip
+            if (line.startsWith("#")) {
+                continue;
+            }
+            //tokenize on ,
+            StringTokenizer csvReader = new StringTokenizer(line, ",");
+            //create new car type from first 5 fields
+            CarType type = new CarType(csvReader.nextToken(),
+                    Integer.parseInt(csvReader.nextToken()),
+                    Float.parseFloat(csvReader.nextToken()),
+                    Double.parseDouble(csvReader.nextToken()),
+                    Boolean.parseBoolean(csvReader.nextToken()));
+            //create N new cars with given type, where N is the 5th field
+            for (int i = Integer.parseInt(csvReader.nextToken()); i > 0; i--) {
+                cars.add(new Car(nextuid++, type));
+            }
+        }
+
+        return cars;
     }
     
     @Override
